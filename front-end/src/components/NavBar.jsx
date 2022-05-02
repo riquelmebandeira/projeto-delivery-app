0e6f991dc2da3d217ee7a5dab67a77a0c52cb804
Guilherme Leao <leaob7@gmail.com>
2022-05-02 16:25:49 -0300

    styles: adiciona estilos iniciais para NavBar

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/NavBar/index.css';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <header>
-      <div>
-        <Link to="/customer/products">
+      <div className="flex-container">
+        <Link to="/customer/products" className="produtos">
           <h5>
             Produtos
           </h5>
@@ -18,12 +19,14 @@ function NavBar() {
         </Link>
       </div>
 
-      <div>
-        <h5>
-          Cicrano da Silva
-        </h5>
+      <div className="flex-container">
+        <div className="name">
+          <h5>
+            Cicrano da Silva
+          </h5>
+        </div>
 
-        <Link to="/login">
+        <Link to="/login" className="logout">
           <h5>
             Sair
           </h5>
