ce156a54430669e9faebcf6e5031c7e2090e6cc9
Guinxard <lucasmfarias3@gmail.com>
2022-05-02 22:37:39 -0300

    :sparkles: fix: corrige propTypes e props de componentes genéricos

--- front-end/src/components/Button.jsx
+++ front-end/src/components/Button.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  const { text, onClick, dataTestId, disabled, placeHolder } = props;
-  return (<button
-    type="button"
-    onClick={onClick}
-    disabled={disabled}
-    data-testid={dataTestId}
-    placeholder={placeHolder}
-  >
-    {text}
-  </button>
+  const { text, onClick, dataTestId, disabled, className } = props;
+  return (
+    <button
+      type="button"
+      onClick={ onClick }
+      disabled={ disabled }
+      data-testid={ dataTestId }
+      className={ className }
+    >
+      { text }
+    </button>
   );
-}
+};
 
-Button.PropTypes = {
+Button.propTypes = {
   text: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   dataTestId: PropTypes.string.isRequired,
   disabled: PropTypes.bool.isRequired,
-  placeHolder: PropTypes.string.isRequired,
-}
+  className: PropTypes.string.isRequired,
+};
 
 export default Button;
--- front-end/src/components/Input.jsx
+++ front-end/src/components/Input.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = (props) => {
-  const { labelText, onChange, dataTestId, placeHolder } = props;
+  const { labelText, onChange, dataTestId, placeHolder, type } = props;
   return (
-    <label htmlFor={labelText}>
-      <h5>{labelText}</h5>
-    <input
-    type="input"
-    onChange={onChange}
-    data-testid={dataTestId}
-    placeholder={placeHolder}
-  />
-  </label>
+    <label htmlFor={ labelText }>
+      <h5>{ labelText }</h5>
+      <input
+        type={ type }
+        onChange={ onChange }
+        data-testid={ dataTestId }
+        placeholder={ placeHolder }
+      />
+    </label>
   );
-}
+};
 
-Input.PropTypes = {
+Input.propTypes = {
   labelText: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   dataTestId: PropTypes.string.isRequired,
   placeHolder: PropTypes.string.isRequired,
-}
+  type: PropTypes.string.isRequired,
+};
 
 export default Input;
