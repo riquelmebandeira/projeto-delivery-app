9e415083e8002294cc4c1ff1f2ca583859201361
Riquelme <riquelmebandeira1@outlook.com>
2022-05-04 19:26:14 -0300

    fix: converts model sale keys to camelcase

--- back-end/src/database/models/sale.js
+++ back-end/src/database/models/sale.js
@@ -1,16 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
   const Sale = sequelize.define('Sale', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    user_id: DataTypes.INTEGER,
-    seller_id: DataTypes.INTEGER,
-    total_price: DataTypes.DECIMAL(9, 2),
-    delivery_address: DataTypes.STRING,
-    delivery_number: DataTypes.STRING,
-    sale_date: DataTypes.DATE,
+    userId: DataTypes.INTEGER,
+    sellerId: DataTypes.INTEGER,
+    totalPrice: DataTypes.DECIMAL(9, 2),
+    deliveryAddress: DataTypes.STRING,
+    deliveryNumber: DataTypes.STRING,
+    saleDate: DataTypes.DATE,
     status: DataTypes.STRING,
-  }, { 
+  }, {
     timestamps: false,
     tableName: 'sales',
+    underscored: true,
   });
   Sale.associate = function (models) {
     Sale.belongsTo(models.User, {
