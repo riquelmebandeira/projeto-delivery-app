d58a096488ebaf09e33c1153933c5c8fa4b2e7aa
Riquelme <riquelmebandeira1@outlook.com>
2022-05-04 17:54:29 -0300

    feat: adds create method in sales service

--- back-end/src/api/Services/saleService.js
+++ back-end/src/api/Services/saleService.js
@@ -1,9 +1,28 @@
-const { Sale } = require('../../database/models');
+const { Sale, SaleProduct } = require('../../database/models');
 
 async function findAll() {
   return Sale.findAll();
 }
 
+async function create(data) {
+  const { products, userId, sellerId, deliveryAddres, deliveryNumber } = data;
+
+  let totalPrice = products.reduce((sum, product) => sum + product.price, 0);
+
+  totalPrice = totalPrice.toFixed(2);
+
+  const newSale = Sale.create({ userId, sellerId, totalPrice, deliveryAddres, deliveryNumber });
+
+  const { id: saleId } = newSale.dataValues;
+
+  const saleProducts = products.map(({ id, quantity }) => ({ saleId, productId: id, quantity }));
+
+  await SaleProduct.bulkCreate(saleProducts);
+
+  return newSale.dataValues;
+}
+
 module.exports = {
   findAll,
+  create,
 };
