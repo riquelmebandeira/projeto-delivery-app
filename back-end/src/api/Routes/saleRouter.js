01ebeacf1e3ce597d7a0af1c7643ec49e64f82ba
Riquelme <riquelmebandeira1@outlook.com>
2022-05-04 18:50:24 -0300

    feat: adds a POST method in sale router

--- back-end/src/api/Routes/saleRouter.js
+++ back-end/src/api/Routes/saleRouter.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
-const validateJWT = require('../../utils/validateJWT');
 
-const { findAll } = require('../Controllers/saleController');
+const validateJWT = require('../../utils/validateJWT');
+const validateSaleData = require('../Middlewares/validateSaleData');
+const { findAll, create } = require('../Controllers/saleController');
 
 const router = Router();
 
 router.get('/', rescue(validateJWT), rescue(findAll));
+router.post('/', rescue(validateSaleData), rescue(create));
 
 module.exports = router;
