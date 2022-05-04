b5e1daf779d5a29d900d176a33a5ddf27709922d
Riquelme <riquelmebandeira1@outlook.com>
2022-05-04 19:02:18 -0300

    fix: allows optional parameters in Joi validation

--- back-end/src/api/Middlewares/validateSaleData.js
+++ back-end/src/api/Middlewares/validateSaleData.js
@@ -1,7 +1,7 @@
 const saleSchema = require('../../Schemas/saleSchema');
 
 module.exports = async (req, res, next) => {
-  const { error } = saleSchema.validate(req.body);
+  const { error } = saleSchema.validate(req.body, { allowUnknown: true });
   
   if (error) return res.status(400).json({ message: error.message });
 
