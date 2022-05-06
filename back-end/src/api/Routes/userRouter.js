96af4562be576667b4487d4cf2219a9a44d6cf6d
Riquelme <riquelmebandeira1@outlook.com>
2022-05-06 09:37:26 -0300

    feat: adds user delete method

--- back-end/src/api/Routes/userRouter.js
+++ back-end/src/api/Routes/userRouter.js
@@ -2,11 +2,12 @@ const { Router } = require('express');
 const rescue = require('express-rescue');
 const validateJWT = require('../../utils/validateJWT');
 
-const { validate, register, findAll } = require('../Controllers/userController');
+const { validate, register, findAll, destroy } = require('../Controllers/userController');
 
 const router = Router();
 
 router.post('/', rescue(validate), rescue(register));
 router.get('/', rescue(validateJWT), rescue(findAll));
+router.delete('/:id', rescue(validateJWT), rescue(destroy));
 
 module.exports = router;
