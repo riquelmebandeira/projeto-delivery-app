52e021c65a5094c0b7e5d1c63ac022f7a6a611f0
Riquelme <riquelmebandeira1@outlook.com>
2022-05-05 22:54:42 -0300

    feat: adds the endpoint GET /sales/:id

--- back-end/src/api/Routes/saleRouter.js
+++ back-end/src/api/Routes/saleRouter.js
@@ -3,11 +3,12 @@ const rescue = require('express-rescue');
 
 const validateJWT = require('../../utils/validateJWT');
 const validateSaleData = require('../Middlewares/validateSaleData');
-const { findAll, create } = require('../Controllers/saleController');
+const { findAll, findOne, create } = require('../Controllers/saleController');
 
 const router = Router();
 
 router.get('/', rescue(validateJWT), rescue(findAll));
+router.get('/:id', rescue(validateJWT), rescue(findOne));
 router.post('/', rescue(validateJWT), rescue(validateSaleData), rescue(create));
 
 module.exports = router;
