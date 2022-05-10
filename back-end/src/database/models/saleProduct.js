582e3e285d8cf14fb8ceb6fe7bae220fc19775f7
riquelme <riquelmebandeira1@outlook.com>
2022-05-10 18:39:50 -0300

    fix(api): altera o nome da tabela de junçao entre vendas e produtos

--- back-end/src/database/models/saleProduct.js
+++ back-end/src/database/models/saleProduct.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   }, { 
     timestamps: false,
     underscored: true,
-    tableName: 'sales_products',
+    tableName: 'salesProducts',
   });
 
   SaleProduct.associate = (models) => {
