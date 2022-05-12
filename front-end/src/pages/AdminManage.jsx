c86c215d59bf6ed43cf921a5fc8829ceb305d3c6
riquelme <riquelmebandeira1@outlook.com>
2022-05-12 19:14:24 -0300

    feat: cria a pagina de gerenciamento

---
+++ front-end/src/pages/AdminManage.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import Button from '../components/Button';
+import NavBar from '../components/NavBar';
+import Select from '../components/Select';
+import Input from '../components/Input';
+import { postOrders as postUser } from '../services/requests';
+import { ROLE_OPTIONS, EMAIL_REGEX, NAME_LENGTH, PWD_LENGTH } from '../utils';
+
+export default function CustomerProducts() {
+  const [sessionUser, setSessionUser] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('seller');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const endpoint = '/users/admin';
+    const { token } = sessionUser;
+
+    try {
+      await postUser(endpoint, token, { name, email, password, role });
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    const userStorage = JSON.parse(localStorage.getItem('user'));
+
+    setSessionUser(userStorage);
+  }, []);
+
+  return (
+    <>
+      {sessionUser && <NavBar props={ sessionUser } />}
+
+      <main>
+        <section>
+          <h4>Cadastrar novo usuário</h4>
+          <form>
+            <Input
+              labelText="Nome"
+              dataTestId="admin_manage__input-name"
+              placeHolder="Nome e sobrenome"
+              onChange={ (e) => setName(e.target.value) }
+            />
+            <Input
+              labelText="Email"
+              dataTestId="admin_manage__input-email"
+              placeHolder="seuemail@site.com"
+              onChange={ (e) => setEmail(e.target.value) }
+            />
+            <Input
+              labelText="Senha"
+              dataTestId="admin_manage__input-password"
+              placeHolder="******"
+              onChange={ (e) => setPassword(e.target.value) }
+            />
+            <Select
+              name="role"
+              labelText="Tipo"
+              dataTestId="admin_manage__select-role"
+              options={ ROLE_OPTIONS }
+              onChange={ (e) => setRole(e.target.value) }
+            />
+            <Button
+              text="CADASTRAR"
+              dataTestId="admin_manage__button-register"
+              disabled={
+                name.length < NAME_LENGTH
+                || !EMAIL_REGEX.test(email)
+                || password.length < PWD_LENGTH
+              }
+              onClick={ handleSubmit }
+            />
+          </form>
+        </section>
+
+        {
+          error && (
+            <h5 data-testid="admin_manage__element-invalid-register">
+              Não foi possível realizar o cadastro
+            </h5>
+          )
+        }
+      </main>
+    </>
+  );
+}
