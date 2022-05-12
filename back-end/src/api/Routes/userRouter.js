443ab4097177259df33c7b028e6d0847613e8465
riquelme <riquelmebandeira1@outlook.com>
2022-05-12 19:01:08 -0300

    feat: adiciona a rota '/users/admin' no router de usuario

--- back-end/src/api/Routes/userRouter.js
+++ back-end/src/api/Routes/userRouter.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
-const validateJWT = require('../../utils/validateJWT');
+const validateJWT = require('../Middlewares/validateJWT');
 
 const { validate, register, findAll,
   destroy, findAllSellers } = require('../Controllers/userController');
@@ -8,6 +8,7 @@ const { validate, register, findAll,
 const router = Router();
 
 router.post('/', rescue(validate), rescue(register));
+router.post('/admin', rescue(validateJWT), rescue(validate), rescue(register));
 router.get('/', rescue(validateJWT), rescue(findAll));
 router.get('/sellers', rescue(validateJWT), rescue(findAllSellers));
 router.delete('/:id', rescue(validateJWT), rescue(destroy));
