3975600bff91b53e77117e6b21b83a10f14589c2
riquelme <riquelmebandeira1@outlook.com>
2022-05-12 18:57:13 -0300

    refactor: move a funçao validateJWT para a pasta de middlewares

---
+++ back-end/src/api/Middlewares/validateJWT.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs/promises');
+const { User } = require('../../database/models');
+const { ERR_CODES, MESSAGES } = require('../../utils/utils');
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(ERR_CODES.UNAUTHORIZED).json({ message: MESSAGES.TOKEN_NOT_FOUND });
+  }
+
+  try {
+    const secret = await fs.readFile('jwt.evaluation.key', 'utf-8');
+
+    const verified = jwt.verify(token, secret);
+
+    const user = await User.findOne({ where: { email: verified.email } });
+
+    if (!user) {  
+      return res
+        .status(ERR_CODES.BAD_REQUEST)
+        .json({ message: MESSAGES.USER_NOT_FOUND });
+    }
+
+    req.user = user.dataValues;
+
+    next();
+  } catch (err) {
+    return res.status(ERR_CODES.BAD_REQUEST).json({ message: MESSAGES.TOKEN_INVALID });
+  }
+};
