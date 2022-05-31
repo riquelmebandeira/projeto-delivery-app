3adf2114afaab3e0a751366b79b89d9f32ca5fdd
riquelme <riquelmebandeira1@outlook.com>
2022-05-31 15:22:30 -0300

    feat(interface): estiliza a pagina de detalhamento de pedido

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Button from '../components/Button';
 import NavBar from '../components/NavBar';
 import Table from '../components/Table';
-import '../styles/pages/CustomerCheckout.css';
+import '../styles/pages/OrderDetails.css';
 import { requestData as requestOrder, updateOrder } from '../services/requests';
 
 export default function OrderDetails() {
@@ -11,6 +11,7 @@ export default function OrderDetails() {
   const [orderDetails, setOrderDetails] = useState({});
   const userStorage = JSON.parse(localStorage.getItem('user'));
   const [orderStatus, setOrderStatus] = useState('Pendente');
+
   useEffect(() => {
     const { token } = JSON.parse(localStorage.getItem('user'));
     setSessionUser(userStorage);
@@ -27,7 +28,8 @@ export default function OrderDetails() {
       }
     };
     getOrderDetails();
-  }, [orderStatus]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const buttonClickHandler = async () => {
     const { token } = JSON.parse(localStorage.getItem('user'));
@@ -45,77 +47,73 @@ export default function OrderDetails() {
   const { role } = sessionUser;
   const dataId = '_order_details__element-order-details-label';
   return (
-    <main>
+    <>
       {sessionUser && <NavBar props={ sessionUser } />}
-      <h4 className="Checkout-Title">
-        Detalhes do pedido
-      </h4>
-      <div className="OrderStatus">
-        Pedido:
-        <h4
-          data-testid={ `${role}${dataId}-order-id` }
-        >
-          {`${id}`}
+      <main>
+        <h4 className="order-details-title">
+          Detalhes do pedido
         </h4>
-        ;
-        <h4>
-          P.Vendedora:
-        </h4>
-        <h4
-          data-testid={ `${role}${dataId}-seller-name` }
-        >
-          {`${seller && seller.name}`}
-          ;
-        </h4>
-        <h4
-          data-testid={ `${role}${dataId}-order-date` }
-        >
-          {new Date(saleDate).toLocaleDateString('pt-BR')}
-        </h4>
-        <h3
-          data-testid={ `${role}${dataId}-delivery-status` }
-        >
-          { orderStatus }
-        </h3>
-        { role === 'seller' && <Button
-          text="Preparar pedido"
-          onClick={ () => buttonClickHandler() }
-          dataTestId="seller_order_details__button-preparing-check"
-          disabled={ orderStatus !== 'Pendente' }
-          className="card-button"
-        /> }
-        { role === 'seller' && <Button
-          text="Saiu para entrega"
-          onClick={ () => buttonClickHandler() }
-          dataTestId="seller_order_details__button-dispatch-check"
-          disabled={ orderStatus !== 'Preparando' }
-          className="card-button"
-        /> }
-        { role === 'customer' && <Button
-          text="Marcar como entregue"
-          onClick={ () => buttonClickHandler() }
-          dataTestId="customer_order_details__button-delivery-check"
-          disabled={ orderStatus !== 'Em Trânsito' }
-          className="card-button"
-        /> }
-      </div>
-      {products && <Table
-        data={ products }
-        columns={
-          ['Item', 'Descrição', 'Quantidade',
-            'Valor Unitário', 'Sub-total']
-        }
-        buttonText={ null }
-        dataTestId="customer_order_details__element-order-table-"
-      />}
-      <div className="card-button">
-        <h3>Total: R$</h3>
-        <h3
-          data-testid={ `${role}_order_details__element-order-total-price` }
-        >
-          {totalPrice && totalPrice.replace(/\./, ',')}
-        </h3>
-      </div>
-    </main>
+        <section className="order-details-container">
+          <div className="order-data-container">
+            <h4
+              data-testid={ `${role}${dataId}-order-id` }
+            >
+              {`PEDIDO: ${id}`}
+            </h4>
+            <h4
+              data-testid={ `${role}${dataId}-seller-name` }
+            >
+              {`P. Vend: ${seller && seller.name}`}
+              ;
+            </h4>
+            <h4
+              data-testid={ `${role}${dataId}-order-date` }
+            >
+              {new Date(saleDate).toLocaleDateString('pt-BR')}
+            </h4>
+            <h3
+              data-testid={ `${role}${dataId}-delivery-status` }
+            >
+              { orderStatus }
+            </h3>
+            { role === 'seller' && <Button
+              text="Preparar pedido"
+              onClick={ () => buttonClickHandler() }
+              dataTestId="seller_order_details__button-preparing-check"
+              disabled={ orderStatus !== 'Pendente' }
+            /> }
+            { role === 'seller' && <Button
+              text="Saiu para entrega"
+              onClick={ () => buttonClickHandler() }
+              dataTestId="seller_order_details__button-dispatch-check"
+              disabled={ orderStatus !== 'Preparando' }
+            /> }
+            { role === 'customer' && <Button
+              text="Marcar como entregue"
+              onClick={ () => buttonClickHandler() }
+              dataTestId="customer_order_details__button-delivery-check"
+              disabled={ orderStatus !== 'Em Trânsito' }
+            /> }
+          </div>
+          {products && <Table
+            data={ products }
+            columns={
+              ['Item', 'Descrição', 'Quantidade',
+                'Valor Unitário', 'Sub-total']
+            }
+            buttonText={ null }
+            dataTestId="customer_order_details__element-order-table-"
+          />}
+          <div className="total-price-container">
+            <h3>Total: R$</h3>
+            <h3
+              data-testid={ `${role}_order_details__element-order-total-price` }
+            >
+              {totalPrice && totalPrice.replace(/\./, ',')}
+            </h3>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
